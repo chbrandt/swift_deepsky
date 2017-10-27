d2f415e2544ef6f5486e8f2a029dec9f138a799c
Carlos Brandt <chbrandt@workland>
2017-10-27 11:55:40 +0200

    Add install script

--- bin/countrates/compile.sh
+++ bin/countrates/compile.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-gfortran -Wall -fno-second-underscore -fcommon -ffixed-line-length-160 -o countrates countrates.f mylib.f
+
+compile(){
+  local HERE=$(cd `dirname $BASH_SOURCE`; pwd)
+  (
+    cd $HERE
+    gfortran -Wall -fno-second-underscore -fcommon -ffixed-line-length-160 \
+            -o countrates countrates.f mylib.f
+  )
+}
+compile
