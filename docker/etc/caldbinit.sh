5df3d0dd1fb4ab522b3f240396665861471d8de9
Carlos H Brandt <carloshenriquebrandt@gmail.com>
2018-11-04 02:25:01 +0100

    Include CALDB volume support adjustments

---
+++ docker/etc/caldbinit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+CALDB="${CALDB:-ftp://legacy.gsfc.nasa.gov/caldb}"
+export CALDB
+export CALDBCONFIG=/caldb/software/tools/caldb.config
+export CALDBALIAS=/caldb/software/tools/alias_config.fits
